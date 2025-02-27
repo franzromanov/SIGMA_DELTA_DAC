5ce9574ccf99a81d109246ec54d33c82bd5bfcce
franzromanov <114895290+franzromanov@users.noreply.github.com>
2025-02-27 21:18:11 +0800

    Create INTEGRATOR_4.v

---
+++ MASH_PARTS/INTEGRATOR_4.v
@@ -0,0 +1,12 @@
+module INTEGRATOR_4 (input wire clk,input wire reset,
+	input wire [3:0] in,	output reg [3:0] out);
+
+	always @(posedge clk) begin
+    	if (reset) begin
+        	out <= 4'b0; 
+    	end else begin
+        	out <= out + in;  // Accumulate the input value
+    	end
+	end
+
+endmodule
