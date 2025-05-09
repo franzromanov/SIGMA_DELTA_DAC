9f574613697e1ef14fcf4748f6a049c8e48d5e65
franzromanov <114895290+franzromanov@users.noreply.github.com>
2025-05-09 13:44:12 +0800

    Update INTEGRATOR_4.v

--- MASH_PARTS/INTEGRATOR_4.v
+++ MASH_PARTS/INTEGRATOR_4.v
@@ -1,7 +1,8 @@
 module INTEGRATOR_4 (input wire clk,input wire reset,
 		     input wire signed [3:0] in,	output reg signed [3:0] out);
 
-	always @(posedge clk) begin
+	
+	always @(posedge clk or posedge rst) begin
     	if (reset) begin
         	out <= 4'b0; 
     	end else begin
