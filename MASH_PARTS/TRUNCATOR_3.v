39b089bc2fd2956984e4d73f4c65c8a2d6afd673
franzromanov <114895290+franzromanov@users.noreply.github.com>
2025-05-09 11:26:24 +0800

    Update TRUNCATOR_3.v

--- MASH_PARTS/TRUNCATOR_3.v
+++ MASH_PARTS/TRUNCATOR_3.v
@@ -1,26 +1,37 @@
 module TRUNCATOR_3(
 	input clck,rst,
-	input signed [15:0]x_in,
+	input signed [3:0]x_in,
 	output reg signed [2:0] y_out,
-	output reg signed [12:0]e_out
+	output reg signed e_out
 
 );
 
 
-	reg signed [15:0]x_in_err;
+	reg signed [4:0]x_in_err;
 
-	always @(posedge clck or posedge rst) begin
-		if (rst) begin
-			x_in_err<=16'd0;
-			e_out<=13'd0;
-			y_out<=3'd0;
-		end else begin
+
+ always@(*)begin
+	 
+		 if(rst==1)begin
+			 x_in_err=5'sd0;
+			 y_out=3'd0;
+		 end else begin
+			 x_in_err = x_in+e_out;
+			 y_out = x_in_err[3:1];
+		 end
+
+	 end
+	 
+
+    always @(posedge clck or posedge rst) begin
 	
-			x_in_err<=x_in+e_out;
-			y_out<=x_in_err[15:13];
-			e_out<=x_in_err[12:0];
-		end
-		
-	end
+         if (rst) begin
+				
+            e_out    <= 1'sd0;
+				
+	 end else if(clck) begin
+				
+		 e_out    <=x_in_err[0];
+	 end
 
 endmodule
